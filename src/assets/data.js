67ed4689bb741da000f63e8070d7aadd0f4567a0
Ananda Fadhilah <perkasa.putra1404@gmail.com>
2024-05-18 03:58:46 +0700

    Create: Portfolio about my self :)

---
+++ src/assets/data.js
@@ -0,0 +1,110 @@
+export const socials = [
+    {
+        logo: 'bx bxl-linkedin-square',
+        link: 'https://www.linkedin.com/in/anandafpp/'
+    },
+    {
+        logo: 'bx bxl-github',
+        link: 'https://github.com/AnandaFPP'
+    },
+    {
+        logo: 'bx bxl-instagram',
+        link: 'https://www.instagram.com/ananda.fpp/'
+    },
+    {
+        logo: 'bx bxl-facebook-square',
+        link: 'https://www.facebook.com/ananda.scroch?locale=id_ID'
+    },
+]
+
+export const info = [
+    { text: 'About' },
+    { text: 'Projects' },
+]
+
+export const technologies = [
+    {
+        text: 'HTML',
+        logo: 'bx bxl-html5'
+    },
+    {
+        text: 'CSS',
+        logo: 'bx bxl-css3'
+    },
+    {
+        text: 'JavaScript',
+        logo: 'bx bxl-javascript'
+    },
+    {
+        text: 'React',
+        logo: 'bx bxl-react'
+    },
+    {
+        text: 'TypeScript',
+        logo: 'bx bxl-typescript'
+    },
+    {
+        text: 'Tailwind',
+        logo: 'bx bxl-tailwind-css'
+    },
+    {
+        text: 'Bootstrap',
+        logo: 'bx bxl-bootstrap'
+    },
+    {
+        text: 'Git',
+        logo: 'bx bxl-git'
+    },
+    {
+        text: 'PostgreSQL',
+        logo: 'bx bxl-postgresql'
+    },
+    {
+        text: 'Redux',
+        logo: 'bx bxl-redux'
+    },
+    {
+        text: 'GitHub',
+        logo: 'bx bxl-github'
+    },
+    {
+        text: 'NodeJS',
+        logo: 'bx bxl-nodejs'
+    },
+]
+
+export const projects = [
+    {
+        title: 'Blanja',
+        description: 'Blanja is an individual project that showcases my skills in both front-end and back-end development. This e-commerce application is designed to provide users with a seamless shopping experience. On the front-end, I used HTML, CSS, and Bootstrap for creating the layout, and React.js to build the dynamic user interface. For the back-end, I implemented an Express.js server and used PostgreSQL for the database. To ensure efficient data retrieval and seamless user interactions, I integrated Axios for API requests.',
+        image: 'blanjaHome',
+        color: '#f04343',
+        hrefgithub: 'https://github.com/AnandaFPP/Blanja-FE',
+        demoLink: 'https://blanja-fe-nine.vercel.app/home'
+    },
+    {
+        title: 'Peworld',
+        description: 'Peworld is a professional networking and job seekers application that I developed individually. This project highlights my expertise in front-end and back-end development. The front-end features a well-designed layout created with HTML, CSS, and Bootstrap, while Next.js was used to enhance the overall framework. On the back-end, I utilized Express.js for server development and PostgreSQL for the database. To facilitate communication between the front-end and back-end, I integrated Axios for smooth data exchange.',
+        image: 'peworldHome',
+        color: '#7334ba',
+        hrefgithub: 'https://github.com/AnandaFPP/Hirejob-FE',
+        demoLink: 'https://hirejob-fe-theta.vercel.app/'
+    },
+    {
+        title: 'MamaRecipe',
+        description: 'MamaRecipe is a collaborative effort in which I played a significant role in backend development. This recipe application is designed to provide users with a wide range of culinary inspirations. For front-end development, we used HTML, CSS, and Bootstrap to create an appealing layout, with React.js enhancing the user experience. On the back-end, I contributed by implementing Express.js and integrating the Supabase database solution to efficiently store and retrieve recipe data. Additionally, I utilized Axios to establish smooth communication between the front-end and back-end, ensuring a seamless user experience.',
+        image: 'mamarecipeHome',
+        color: '#d6c636',
+        hrefgithub: 'https://github.com/itsarsile/food_recipe',
+        demoLink: 'https://food-recipe-kel2.vercel.app/home'
+    },
+];
+
+export const buttons = [
+    {
+        github: 'bx bxl-github',
+        demo: 'bx bx-desktop',
+        back: 'bx bxs-left-arrow-circle',
+    }
+]
+
---
+++ src/assets/index.js
@@ -0,0 +1,56 @@
+document.addEventListener('astro:page-load', () => {
+    // Slider
+    const wrapper = document.querySelector('[data-tech-wrapper]');
+    let copy = document.querySelector('.tech-card').cloneNode(true);
+    wrapper.append(copy)
+
+    const modal = document.querySelector('[data-tech-modal]');
+    const h1 = document.querySelector('[data-tech-h1]');
+    const stop = document.querySelector('[data-tech-stop]');
+
+    h1.addEventListener('click', () => {
+        modal.classList.toggle('hidden')
+        modal.classList.add('flex')
+    })
+
+    wrapper.addEventListener('click', () => {
+        modal.classList.toggle('hidden');
+        modal.classList.add('flex');
+    })
+
+    modal.addEventListener('click', () => {
+        modal.classList.remove('flex');
+        modal.classList.toggle('hidden');
+    })
+
+    stop.addEventListener('click', (e) => e.stopPropagation())
+
+    addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            modal.classList.remove('flex');
+            modal.classList.toggle('hidden')
+        }
+    })
+})
+
+// Email JS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.textContent = 'Sending...';
+
+        const serviceID = 'default_service';
+        const templateID = 'template_mk6lwq6';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.textContent = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.textContent = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+    });
